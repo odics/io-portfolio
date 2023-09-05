eebe59c3bae5772b46a3022d89a03859ffecd48d
odics <odics@vcu.edu>
2023-09-05 14:52:59 +0100

    Chevron animation for read more button

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 const readMore = document.getElementById("readmore");
 const readMore2 = document.getElementById("readmore2");
 
+const arrow1 = document.getElementById("arrow1");
+const arrow2 = document.getElementById("arrow2");
+
 const moreContainer = document.getElementById("more-text");
 const moreContainer2 = document.getElementById("more-text2");
 
 readMore.addEventListener("click", () => {
   moreContainer.classList.toggle("active");
+  arrow1.classList.toggle("rotate-up");
 });
 
 readMore2.addEventListener("click", () => {
   moreContainer2.classList.toggle("active");
+  arrow2.classList.toggle("rotate-up");
 });
